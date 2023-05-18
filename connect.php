34373e08101e5aeebd4b779ec4ff47ffaaa454a1
Madan-26 <117678430+Madan-26@users.noreply.github.com>
2023-05-18 19:45:07 +0530

    Add files via upload

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'POST') {
     
     $firstName = $_POST["firstname"];
     $lastName = $_POST["lastname"];
